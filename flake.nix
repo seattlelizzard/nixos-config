f7053fba209168e2da1c296981b67d84af80fdd4
= <=>
2024-09-18 17:07:18 -0700

    removing old vim stuff

--- flake.nix
+++ flake.nix
@@ -4,25 +4,19 @@
   inputs = {
     # NixOS official package source, using the nixos-23.11 branch here
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
-    khanelivim.url = "github:khaneliman/khanelivim";
-
+    #neovim.url = "github:neovim/neovim/master";
   };
 
-  outputs = { self, nixpkgs, khanelivim, ... }@inputs: {
+  outputs = { self, nixpkgs, ... }@inputs: {
     # Please replace my-nixos with your hostname
     nixosConfigurations.lizzardlix = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
+      specialArgs = { inherit inputs; };
       modules = [
         # Import the previous configuration.nix we used,
         # so the old configuration file still takes effect
         ./configuration.nix
       ];
     };
-    #packages = with nixpkgs; [
-    #  khanelivim.packages.x86_64-linux.default
-    #];
-    devShells.default = nixpkgs.mkShell {
-        nativeBuildInputs = [ khanelivim.packages.x86_64-linux.default ];
-      };
   };
 }
