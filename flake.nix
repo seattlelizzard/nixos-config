576a168ad3c668b0dc774404b5326207350cbd31
= <=>
2024-09-20 18:39:34 -0700

    Adding flake input

--- flake.nix
+++ flake.nix
@@ -5,9 +5,10 @@
     # NixOS official package source, using the nixos-23.11 branch here
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
     #neovim.url = "github:neovim/neovim/master";
+    lizzvim.url = "github:seattlelizzard/nix-vim/0.1.0";
   };
 
-  outputs = { self, nixpkgs, ... }@inputs: {
+  outputs = { self, nixpkgs, lizzvim, ... }@inputs: {
     # Please replace my-nixos with your hostname
     nixosConfigurations.lizzardlix = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
